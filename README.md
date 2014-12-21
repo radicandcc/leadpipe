fd3aa36c1d6ea748434f7b670c106c9ea004ca85
Anthony Shull <anthony@rockriverstar.com>
2014-12-21 11:13:21 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# leadpipe
+
+Col. Mustard did it in the conservatory with a leadpipe.
+
+## About
+
+Track and manage leadership development.
+
+## License
+
+Copyright © 2014 Radicand
---
+++ src/leadpipe/views/layout.clj
@@ -0,0 +1,9 @@
+(ns leadpipe.views.layout
+  (:require [hiccup.page :refer [html5 include-css]]))
+
+(defn common [& body]
+  (html5
+    [:head
+     [:title "Welcome to leadpipe"]
+     (include-css "/css/screen.css")]
+    [:body body]))
