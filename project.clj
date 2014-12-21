4e7ef2f4f7cc3babec7f87ea8e0ed53bcfcb09c1
Anthony Shull <anthony@rockriverstar.com>
2014-12-21 12:33:32 -0500

    Added liberator, cheshire, and neocons dependencies to project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [liberator "0.12.2"]
+                 [cheshire "5.4.0"]
+                 [clojurewerkz/neocons "3.0.0"]]
+
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler leadpipe.handler/app
          :init leadpipe.handler/init
